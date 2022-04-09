ef0355b4e5acb1189f07ac016e517cfd110a01a9
Cora Iberkleid <ciberkleid@gmail.com>
2022-04-09 16:16:32 -0400

    Accept message via env var or request URL

--- hello-server.go
+++ hello-server.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strings"
 )
 
+// global variables
+var defaultMsg string = getEnv("HELLO_MSG", "sunshine")
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    log.Print("Request received")
+    msg := defaultMsg
+    uriSegments := strings.Split(r.URL.Path, "/")
+    if uriSegments[1] != "" {
+        msg = uriSegments[1]
+	log.Print("Request message: [%s]", msg)
+    }
+    fmt.Fprintf(w, "<h1>Hello %s!</h1>\n", msg)
+}
+
 func main() {
-	fmt.Printf("Env var: %s = %s \n", "environment", os.Getenv("environment"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, sunshine!\n")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Print("Server started. Default message: [%s]", defaultMsg)
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", handler)
+    mux.HandleFunc("/*", handler)
+    log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
 }
